debb55f16d28353cf245b379e6b379b0c7c270a5
Chonghuan.Wang <ch.wang@ennovationtech.eu>
2025-01-01 23:33:49 +0000

    stage 4, supervisor multi agent pattern

--- tuto9_customer_support_bot/agent.py
+++ tuto9_customer_support_bot/agent.py
@@ -6,8 +6,8 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import Runnable, RunnableConfig
 from typing_extensions import TypedDict
 from langchain_openai import ChatOpenAI
-
 from langgraph.graph.message import AnyMessage, add_messages
+from pydantic import BaseModel, Field
 
 from tools_lookup_company_policies import lookup_policy
 from tools_car_rental import (
@@ -36,6 +36,8 @@ from tools_hotels import (
 )
 from state import State
 
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=1)
+
 class Assistant:
     def __init__(self, runnable: Runnable):
         self.runnable = runnable
@@ -43,8 +45,7 @@ class Assistant:
     def __call__(self, state: State, config: RunnableConfig):
         while True:
             result = self.runnable.invoke(state)
-            # If the LLM happens to return an empty response, we will re-prompt it
-            # for an actual response.
+
             if not result.tool_calls and (
                 not result.content
                 or isinstance(result.content, list)
@@ -56,52 +57,266 @@ class Assistant:
                 break
         return {"messages": result}
 
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=1)
 
-assistant_prompt = ChatPromptTemplate.from_messages(
+class CompleteOrEscalate(BaseModel):
+    """A tool to mark the current task as completed and/or to escalate control of the dialog to the main assistant,
+    who can re-route the dialog based on the user's needs."""
+
+    cancel: bool = True
+    reason: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "cancel": True,
+                "reason": "User changed their mind about the current task.",
+            },
+            "example 2": {
+                "cancel": True,
+                "reason": "I have fully completed the task.",
+            },
+            "example 3": {
+                "cancel": False,
+                "reason": "I need to search the user's emails or calendar for more information.",
+            },
+        }
+
+
+# Flight booking assistant
+
+flight_booking_prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "You are a helpful customer support assistant for Swiss Airlines. "
-            " Use the provided tools to search for flights, company policies, and other information to assist the user's queries. "
+            "You are a specialized assistant for handling flight updates. "
+            " The primary assistant delegates work to you whenever the user needs help updating their bookings. "
+            "Confirm the updated flight details with the customer and inform them of any additional fees. "
             " When searching, be persistent. Expand your query bounds if the first search returns no results. "
-            " If a search comes up empty, expand your search before giving up."
-            "\n\nCurrent user:\n<User>\n{user_info}\n</User>"
-            "\nCurrent time: {time}.",
+            "If you need more information or the customer changes their mind, escalate the task back to the main assistant."
+            " Remember that a booking isn't completed until after the relevant tool has successfully been used."
+            "\n\nCurrent user flight information:\n<Flights>\n{user_info}\n</Flights>"
+            "\nCurrent time: {time}."
+            "\n\nIf the user needs help, and none of your tools are appropriate for it, then"
+            ' "CompleteOrEscalate" the dialog to the host assistant. Do not waste the user\'s time. Do not make up invalid tools or functions.',
         ),
         ("placeholder", "{messages}"),
     ]
 ).partial(time=datetime.now)
 
+update_flight_safe_tools = [search_flights]
+update_flight_sensitive_tools = [update_ticket_to_new_flight, cancel_ticket]
+update_flight_tools = update_flight_safe_tools + update_flight_sensitive_tools
+update_flight_runnable = flight_booking_prompt | llm.bind_tools(
+    update_flight_tools + [CompleteOrEscalate]
+)
 
-# "Read"-only tools (such as retrievers) don't need a user confirmation to use
-part_3_safe_tools = [
-    TavilySearchResults(max_results=1),
-    fetch_user_flight_information,
-    search_flights,
-    lookup_policy,
-    search_car_rentals,
-    search_hotels,
-    search_trip_recommendations,
-]
+# Hotel Booking Assistant
+book_hotel_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a specialized assistant for handling hotel bookings. "
+            "The primary assistant delegates work to you whenever the user needs help booking a hotel. "
+            "Search for available hotels based on the user's preferences and confirm the booking details with the customer. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            "If you need more information or the customer changes their mind, escalate the task back to the main assistant."
+            " Remember that a booking isn't completed until after the relevant tool has successfully been used."
+            "\nCurrent time: {time}."
+            '\n\nIf the user needs help, and none of your tools are appropriate for it, then "CompleteOrEscalate" the dialog to the host assistant.'
+            " Do not waste the user's time. Do not make up invalid tools or functions."
+            "\n\nSome examples for which you should CompleteOrEscalate:\n"
+            " - 'what's the weather like this time of year?'\n"
+            " - 'nevermind i think I'll book separately'\n"
+            " - 'i need to figure out transportation while i'm there'\n"
+            " - 'Oh wait i haven't booked my flight yet i'll do that first'\n"
+            " - 'Hotel booking confirmed'",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now)
 
-# These tools all change the user's reservations.
-# The user has the right to control what decisions are made
-part_3_sensitive_tools = [
-    update_ticket_to_new_flight,
-    cancel_ticket,
+book_hotel_safe_tools = [search_hotels]
+book_hotel_sensitive_tools = [book_hotel, update_hotel, cancel_hotel]
+book_hotel_tools = book_hotel_safe_tools + book_hotel_sensitive_tools
+book_hotel_runnable = book_hotel_prompt | llm.bind_tools(
+    book_hotel_tools + [CompleteOrEscalate]
+)
+
+# Car Rental Assistant
+book_car_rental_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a specialized assistant for handling car rental bookings. "
+            "The primary assistant delegates work to you whenever the user needs help booking a car rental. "
+            "Search for available car rentals based on the user's preferences and confirm the booking details with the customer. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            "If you need more information or the customer changes their mind, escalate the task back to the main assistant."
+            " Remember that a booking isn't completed until after the relevant tool has successfully been used."
+            "\nCurrent time: {time}."
+            "\n\nIf the user needs help, and none of your tools are appropriate for it, then "
+            '"CompleteOrEscalate" the dialog to the host assistant. Do not waste the user\'s time. Do not make up invalid tools or functions.'
+            "\n\nSome examples for which you should CompleteOrEscalate:\n"
+            " - 'what's the weather like this time of year?'\n"
+            " - 'What flights are available?'\n"
+            " - 'nevermind i think I'll book separately'\n"
+            " - 'Oh wait i haven't booked my flight yet i'll do that first'\n"
+            " - 'Car rental booking confirmed'",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now)
+
+book_car_rental_safe_tools = [search_car_rentals]
+book_car_rental_sensitive_tools = [
     book_car_rental,
     update_car_rental,
     cancel_car_rental,
-    book_hotel,
-    update_hotel,
-    cancel_hotel,
-    book_excursion,
-    update_excursion,
-    cancel_excursion,
 ]
-sensitive_tool_names = {t.name for t in part_3_sensitive_tools}
-# Our LLM doesn't have to know which nodes it has to route to. In its 'mind', it's just invoking functions.
-part_3_assistant_runnable = assistant_prompt | llm.bind_tools(
-    part_3_safe_tools + part_3_sensitive_tools
+book_car_rental_tools = book_car_rental_safe_tools + book_car_rental_sensitive_tools
+book_car_rental_runnable = book_car_rental_prompt | llm.bind_tools(
+    book_car_rental_tools + [CompleteOrEscalate]
+)
+
+# Excursion Assistant
+
+book_excursion_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a specialized assistant for handling trip recommendations. "
+            "The primary assistant delegates work to you whenever the user needs help booking a recommended trip. "
+            "Search for available trip recommendations based on the user's preferences and confirm the booking details with the customer. "
+            "If you need more information or the customer changes their mind, escalate the task back to the main assistant."
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            " Remember that a booking isn't completed until after the relevant tool has successfully been used."
+            "\nCurrent time: {time}."
+            '\n\nIf the user needs help, and none of your tools are appropriate for it, then "CompleteOrEscalate" the dialog to the host assistant. Do not waste the user\'s time. Do not make up invalid tools or functions.'
+            "\n\nSome examples for which you should CompleteOrEscalate:\n"
+            " - 'nevermind i think I'll book separately'\n"
+            " - 'i need to figure out transportation while i'm there'\n"
+            " - 'Oh wait i haven't booked my flight yet i'll do that first'\n"
+            " - 'Excursion booking confirmed!'",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now)
+
+book_excursion_safe_tools = [search_trip_recommendations]
+book_excursion_sensitive_tools = [book_excursion, update_excursion, cancel_excursion]
+book_excursion_tools = book_excursion_safe_tools + book_excursion_sensitive_tools
+book_excursion_runnable = book_excursion_prompt | llm.bind_tools(
+    book_excursion_tools + [CompleteOrEscalate]
+)
+
+
+# Primary Assistant
+class ToFlightBookingAssistant(BaseModel):
+    """Transfers work to a specialized assistant to handle flight updates and cancellations."""
+
+    request: str = Field(
+        description="Any necessary followup questions the update flight assistant should clarify before proceeding."
+    )
+
+
+class ToBookCarRental(BaseModel):
+    """Transfers work to a specialized assistant to handle car rental bookings."""
+
+    location: str = Field(
+        description="The location where the user wants to rent a car."
+    )
+    start_date: str = Field(description="The start date of the car rental.")
+    end_date: str = Field(description="The end date of the car rental.")
+    request: str = Field(
+        description="Any additional information or requests from the user regarding the car rental."
+    )
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "location": "Basel",
+                "start_date": "2023-07-01",
+                "end_date": "2023-07-05",
+                "request": "I need a compact car with automatic transmission.",
+            }
+        }
+
+
+class ToHotelBookingAssistant(BaseModel):
+    """Transfer work to a specialized assistant to handle hotel bookings."""
+
+    location: str = Field(
+        description="The location where the user wants to book a hotel."
+    )
+    checkin_date: str = Field(description="The check-in date for the hotel.")
+    checkout_date: str = Field(description="The check-out date for the hotel.")
+    request: str = Field(
+        description="Any additional information or requests from the user regarding the hotel booking."
+    )
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "location": "Zurich",
+                "checkin_date": "2023-08-15",
+                "checkout_date": "2023-08-20",
+                "request": "I prefer a hotel near the city center with a room that has a view.",
+            }
+        }
+
+
+class ToBookExcursion(BaseModel):
+    """Transfers work to a specialized assistant to handle trip recommendation and other excursion bookings."""
+
+    location: str = Field(
+        description="The location where the user wants to book a recommended trip."
+    )
+    request: str = Field(
+        description="Any additional information or requests from the user regarding the trip recommendation."
+    )
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "location": "Lucerne",
+                "request": "The user is interested in outdoor activities and scenic views.",
+            }
+        }
+
+
+# The top-level assistant performs general Q&A and delegates specialized tasks to other assistants.
+# The task delegation is a simple form of semantic routing / does simple intent detection
+
+primary_assistant_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful customer support assistant for Swiss Airlines. "
+            "Your primary role is to search for flight information and company policies to answer customer queries. "
+            "If a customer requests to update or cancel a flight, book a car rental, book a hotel, or get trip recommendations, "
+            "delegate the task to the appropriate specialized assistant by invoking the corresponding tool. You are not able to make these types of changes yourself."
+            " Only the specialized assistants are given permission to do this for the user."
+            "The user is not aware of the different specialized assistants, so do not mention them; just quietly delegate through function calls. "
+            "Provide detailed information to the customer, and always double-check the database before concluding that information is unavailable. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            " If a search comes up empty, expand your search before giving up."
+            "\n\nCurrent user flight information:\n<Flights>\n{user_info}\n</Flights>"
+            "\nCurrent time: {time}.",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now)
+primary_assistant_tools = [
+    TavilySearchResults(max_results=1),
+    search_flights,
+    lookup_policy,
+]
+assistant_runnable = primary_assistant_prompt | llm.bind_tools(
+    primary_assistant_tools
+    + [
+        ToFlightBookingAssistant,
+        ToBookCarRental,
+        ToHotelBookingAssistant,
+        ToBookExcursion,
+    ]
 )
